612bb8485e0a216d001a6e8e39024588d4a3447c
Jacob Fromm <jfromm@yext.com>
2023-01-31 16:36:33 -0500

    generating array of fifty random words

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { arrayBuffer } from "stream/consumers";
 import words from '../util/just-words.json'
 export default function App() {
     console.log("word list: ", words)
+   const fiftyRandomWords = (words: []) => {
+    let els = generateElements()
+    return words.slice(els[0], els[1])
+   }
+
+   const generateElements = () => {
+    let firstNum = Math.floor(Math.random() * 10000)
+    let secondNum
+    if (10000 - firstNum < 50) {
+        secondNum = firstNum - 50
+        console.log("first higher than second", [secondNum, firstNum])
+        return [secondNum, firstNum]
+    } else {
+        secondNum = firstNum + 50
+        console.log( "second higher than first", [firstNum, secondNum])
+        return [firstNum, secondNum]
+    }
+   }
+
+   console.log("random words", fiftyRandomWords(words))
   return (
     <>
       <h1>this is where we'll build the app</h1>

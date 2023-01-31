3a55802a4092bab4ea08fbb329ad5006af73d9d1
Jacob Fromm <jfromm@yext.com>
2023-01-31 16:54:23 -0500

    words rendering on page in app.tsx

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,31 +1,50 @@
 import React from "react";
 import { arrayBuffer } from "stream/consumers";
-import words from '../util/just-words.json'
+import wordsList from '../util/just-words.json'
+console.log("word list: ", wordsList)
+
+export const fiftyRandomWords = (words: []) => {
+ let els = generateElements()
+ let wordList = words.slice(els[0], els[1])
+ return wordList
+}
+
+const generateElements = () => {
+ let firstNum = Math.floor(Math.random() * 10000)
+ let secondNum
+ if (10000 - firstNum < 50) {
+     secondNum = firstNum - 50
+     console.log("first higher than second", [secondNum, firstNum])
+     return [secondNum, firstNum]
+ } else {
+     secondNum = firstNum + 50
+     console.log( "second higher than first", [firstNum, secondNum])
+     return [firstNum, secondNum]
+ }
+}
+
+interface WordList {
+    words: []
+}
+
+interface WordListProps {
+    words: WordList
+}
+
+console.log("random words", fiftyRandomWords(wordsList))
+
 export default function App() {
-    console.log("word list: ", words)
-   const fiftyRandomWords = (words: []) => {
-    let els = generateElements()
-    return words.slice(els[0], els[1])
-   }
-
-   const generateElements = () => {
-    let firstNum = Math.floor(Math.random() * 10000)
-    let secondNum
-    if (10000 - firstNum < 50) {
-        secondNum = firstNum - 50
-        console.log("first higher than second", [secondNum, firstNum])
-        return [secondNum, firstNum]
-    } else {
-        secondNum = firstNum + 50
-        console.log( "second higher than first", [firstNum, secondNum])
-        return [firstNum, secondNum]
-    }
-   }
-
-   console.log("random words", fiftyRandomWords(words))
-  return (
+let words = fiftyRandomWords(wordsList)
+console.log("words in function", words)
+    return (
     <>
       <h1>this is where we'll build the app</h1>
+      <ol>
+        {words.map(word => {return(
+            <li key={word}>{word}</li>
+        )
+        })}
+      </ol>
     </>
   );
 }

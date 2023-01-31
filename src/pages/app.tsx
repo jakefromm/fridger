423281b693b1ae508b30b7f5d07566e5fb668349
Jacob Fromm <jfromm@yext.com>
2023-01-31 16:02:55 -0500

    formatted and imported word list which is now logging in app.tsx

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-
+import words from '../util/just-words.json'
 export default function App() {
+    console.log("word list: ", words)
   return (
     <>
       <h1>this is where we'll build the app</h1>
